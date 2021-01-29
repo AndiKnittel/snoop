491b70c87fdd33c29a4e99c2d8e1569bf749569e
Stefanibus <stefan.brueck@posteo.de>
2021-01-29 12:24:37 +0100

    initial api request, added axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {useEffect, useState} from 'react';  
 import Search from './components/Search';
 import SelectedRestaurant from './components/SelectedRestaurant';
 import FilteredRestaurants from './components/FilteredRestaurants';
+import axios from 'axios';
+
+ 
 
 function App() {
+
+const [restaurants, setRestaurants] = useState([]);
+const [city, setCity] = useState('');
+const [type, setType] = useState('');
+
+ 
+useEffect(() => {
+
+	axios.get('https://secret-cove-78238.herokuapp.com/restaurant')
+	.then((res) => {
+		// console.log('res: ', res);
+		setRestaurants(res.data.data)
+	})
+	.catch((err) => {
+		console.error(err); 	
+	}) 
+},[]) 
+
+  console.log('restaurants: ', restaurants);
+		
   return (
     <div className="App">
       <Search />
@@ -14,3 +38,5 @@ function App() {
 }
 
 export default App;
+
+ 
